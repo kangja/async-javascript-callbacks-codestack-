e7abf5b1c2f921d9f60262aa8e7cfb5bd2019ab6
Jason <kangja1996@gmail.com>
2021-07-31 01:06:13 -0400

    finished

--- main.js
+++ main.js
@@ -27,10 +27,11 @@
 // userInfo("John", "Doe", greeting);
 
 const hasMeeting = true;
+
 const meeting = new Promise((resolve, reject) => {
   if (!hasMeeting) {
     const meetingDetails = {
-      name: "marking meeting",
+      name: "marketinging meeting",
       location: "skype",
       time: "1 pm",
     };
@@ -40,13 +41,75 @@ const meeting = new Promise((resolve, reject) => {
   }
 });
 
-meeting
-  .then((res) => {
-    // resolve data
-    console.log("meeting scheduled");
-    console.log(res);
-  })
-  .catch((err) => {
-    // reject data
+// const addToCalendar = meetingDetails => {
+//     return new Promise((resolve, reject) => {
+//         const calendar = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
+//         resolve(calendar);
+// })
+// }
+
+// const addToCalendar = (meetingDetails) => {
+//   const calendar = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
+//   return Promise.resolve(calendar);
+// };
+
+// meeting
+//   .then(addToCalendar)
+//   .then((res) => {
+//     // resolve data
+//     console.log("meeting scheduled");
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     // reject data
+//     console.log(err.message);
+//   });
+
+// const promise1 = Promise.resolve("Promise 1 complete");
+
+// const promise2 = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("Promise 2 complete");
+//   }, 2000);
+// });
+
+// promise1.then((res) => console.log(res));
+// promise2.then((res) => console.log(res));
+
+// Promise.all([promise1, promise2]).then((res) => console.log(res));
+// Promise.race([promise1, promise2]).then((res) => console.log(res));
+
+// function* genertorFunc() {
+//   let data = getData();
+//   yield data;
+//   console.log(data);
+// }
+
+const addToCalendar = (meetingDetails) => {
+  const calendar = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
+  return Promise.resolve(calendar);
+};
+
+async function myMeeting() {
+  try {
+    const meetingDetails = await meeting;
+    const message = await addToCalendar(meetingDetails);
+    console.log(message);
+  } catch (err) {
     console.log(err.message);
-  });
+  }
+  //   meeting
+  //     .then(addToCalendar)
+  //     .then((res) => {
+  //       // resolve data
+  //       console.log("meeting scheduled");
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       // reject data
+  //       console.log(err.message);
+  //     });
+}
+myMeeting();
+
+// myMeeting().catch((err) => console.log(err.message));
